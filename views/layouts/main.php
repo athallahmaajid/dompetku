8f45beca0f819014f17abe6b805e0f58742b5bf3
athallahmaajid <athallahmaajid@gmail.com>
2021-06-08 10:49:54 +0700

    clean up unrelated application

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -35,26 +35,6 @@ AppAsset::register($this);
             'class' => 'navbar navbar-fixed-top navbar-expand-lg navbar-dark bg-dark',
         ],
     ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right ml-auto'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
-    ]);
     NavBar::end();
     ?>
 
